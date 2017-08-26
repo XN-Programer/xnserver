6a613379d6aa47933b27a6ef0502d6f39c4a6fbd
KingbellX <1017990427@qq.com>
2017-08-26 12:19:38 +0800

    数据库配置&报修user页面迁入完成

---
+++ Application/Home/Controller/NetController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Home\Controller\CommonController;
+class NetController extends CommonController{
+    public function index(){
+        $this->display();
+    }
+}
